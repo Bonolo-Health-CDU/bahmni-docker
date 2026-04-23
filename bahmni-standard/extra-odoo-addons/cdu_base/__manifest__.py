7d9eec3cb684ebe3914d29543d51aaf5ed14b18b
lmphatsi <la.mphatsi@gmail.com>
2026-04-23 11:10:47 +0200

    initial  update to the skin and logos

---
+++ bahmni-standard/extra-odoo-addons/cdu_base/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "CDU Base",
+    "summary": "Central Dispensing Unit branding customizations",
+    "version": "16.0.1.0.0",
+    "category": "Localization",
+    "author": "Ministry of Health",
+    "license": "LGPL-3",
+    "depends": ["base", "web"],
+    "data": [
+        "views/login_template.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "cdu_base/static/src/css/custom_backend.css",
+        ],
+        "web.assets_frontend": [
+            "cdu_base/static/src/css/custom_frontend.css",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
